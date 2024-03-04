63fcb65871a793e6e45ef2c447f3020f89872b7a
Matthias <matthias.prieto@redsalud.gob.cl>
2024-03-04 17:30:58 -0300

    subida

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, heroesApp');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('heroesApp app is running!');
   });
 });
